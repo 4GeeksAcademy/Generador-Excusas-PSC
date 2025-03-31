edfba5b7667729bfc115282a23db11c9b717c43d
PRIMED587 <priveram87@gmail.com>
2025-03-31 20:11:40 +0000

    Actualizacion

--- src/app.js
+++ src/app.js
@@ -1,11 +1,20 @@
-import "bootstrap";
-import "./style.css";
 
+let quien = ['Maria', 'Juan', 'Fulano', 'Perencejo', 'Mengano'];
+let accion = ['rompio', 'aplastó', 'rayo', 'ensucio', 'acomodo'];
+let objeto = ['el auto', 'la casa', 'la bici', 'la moto', 'el computador']
+let cuando = ['Empezando el dia', 'En la noche', 'durante el almuerzo', 'el fin de semana'];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+function genEx(quien, accion, objeto, cuando) {
+    numquien = Math.floor(Math.random() * quien.length);
+    numque = Math.floor(Math.random() * accion.length);
+    numobjeto = Math.floor(Math.random() * objeto.length);
+    numcuando = Math.floor(Math.random() * cuando.length);
+    return quien[numquien] + " " + accion[numque] + " " + objeto[numobjeto] + " " + cuando[numcuando];
+}
+function cargar() {
+    var excusahtml = document.getElementById("excusahtml");
+    excusahtml.innerHTML = genEx(quien, accion, objeto, cuando);
+}
+cargar();
+console.log(cargar);
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
